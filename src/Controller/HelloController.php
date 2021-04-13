25bfa53fda65006f61c2e50fd6a5a0248e35fbcf
mathieu bonhommeau <mat.bonhommeau85@gmail.com>
2021-04-13 12:10:22 +0200

    first commit

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloController extends AbstractController
+{
+    /**
+     * @Route("/hello", name="hello")
+     */
+    public function index(): Response
+    {
+        echo "Hello world !";
+        return $this->render('hello/index.html.twig', [
+            'controller_name' => 'HelloController',
+        ]);
+    }
+}
